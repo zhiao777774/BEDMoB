8ad87874de5980fd51157aefbf48be01e27ce9cb
zhiao777774 <zhiaohsu@gmail.com>
2021-04-03 21:58:13 +0800

    初始化utils資料夾

---
+++ utils/date.js
@@ -0,0 +1,45 @@
+export function lastDay(y, m, index = true) {
+    return new Date(y, m + (index ? 1 : 0), 0).getDate();
+}
+
+export function format(fmt) {
+    const o = {
+        'M+': this.getMonth() + 1, //月份
+        'd+': this.getDate(), //日
+        'h+': this.getHours(), //小時
+        'm+': this.getMinutes(), //分
+        's+': this.getSeconds(), //秒
+        'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+        'S': this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    for (let k in o)
+        if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+    return fmt;
+}
+
+export function isSameDay(d1, d2) {
+    return d1.getFullYear() === d2.getFullYear() &&
+        d1.getDate() === d2.getDate() &&
+        d1.getMonth() === d2.getMonth();
+}
+
+export function addDays(days) {
+    const date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+export function getDatesBetween(startDate, endDate) {
+    const dateArray = [];
+    let currentDate = startDate;
+    while (currentDate <= endDate) {
+        dateArray.push(new Date(currentDate));
+        currentDate = currentDate.addDays(1);
+    }
+    return dateArray;
+}
+
+export function getDiffDays(startDate, endDate) {
+    return startDate - endDate / (1000 * 3600 * 24);
+}
