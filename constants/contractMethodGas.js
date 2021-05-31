99b607754295fc4134584641e41e79d97f14ebfb
zhiao777774 <zhiaohsu@gmail.com>
2021-05-31 18:37:31 +0800

    SmartContract各方法需消耗的Gas

---
+++ constants/contractMethodGas.js
@@ -0,0 +1,8 @@
+export default {
+    dataOwnerCreateContentProduct: 281728,
+    consumerRegisterProduct: 148694,
+    purchaseProductContent: 75793,
+    sendProductContent: 86491,
+    queryProductContent: 281728,
+    getProductInfo: 28018
+};
