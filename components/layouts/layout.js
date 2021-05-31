730bb434c9ed77497467d95d36803e177caa00d2
zhiao777774 <zhiaohsu@gmail.com>
2021-06-01 02:43:53 +0800

    調整預設tittle名稱

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -3,7 +3,7 @@ import Navbar from '@/components/navbar';
 import PageTopButton from '@/components/pagetop';
 
 
-export default function Layout({ children, title = 'BEPDPP', user = undefined }) {
+export default function Layout({ children, title = 'BEDMoB', user = undefined }) {
     return (
         <div>
             <Head>
