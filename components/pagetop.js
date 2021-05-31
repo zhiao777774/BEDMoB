6f28ace546373a80b0c69f8f8d4213c00dea5d70
zhiao777774 <zhiaohsu@gmail.com>
2021-05-31 18:37:45 +0800

    優化程式碼

--- components/pagetop.js
+++ components/pagetop.js
@@ -1,19 +1,27 @@
 import { Component } from 'react';
-import { withRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 
-class PageTopButton extends Component {
+export default class PageTopButton extends Component {
     constructor(props) {
         super(props);
         this.state = { showPageTop: false };
     }
 
-    _removeAnchors = () => {
-        const { router } = this.props;
-        router.push(router.asPath.split('#')[0]);
-    };
+    _jumpToPageTop(e) {
+        e.preventDefault();
+
+        const { target } = e;
+        const href = target.getAttribute('href') ||
+            target.parentNode.getAttribute('href') ||
+            target.parentNode.parentNode.getAttribute('href');
+
+        document.querySelector(href).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
 
     componentDidMount() {
         document.addEventListener('scroll', () => {
@@ -26,11 +34,9 @@ class PageTopButton extends Component {
     render() {
         return (
             <a className={'scroll-to-top rounded hover:bg-gray-700 ' + (this.state.showPageTop ? 'block' : 'hidden')}
-                href="#page-top" onClick={this._removeAnchors} >
+                href="#page-top" onClick={this._jumpToPageTop} >
                 <FontAwesomeIcon icon={faAngleUp} size="lg" />
             </a>
         );
     }
 }
-
-export default withRouter(PageTopButton);
