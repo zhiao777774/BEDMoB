f0919576fc5a4f52f66e9d94e6ef28328f92a2de
zhiao777774 <zhiaohsu@gmail.com>
2021-06-01 02:45:10 +0800

    新增連結

--- components/navbar.js
+++ components/navbar.js
@@ -11,10 +11,7 @@ export default class Navbar extends Component {
 
         this.state = {
             openPanel: false,
-            searchRecord: [
-                'inspire',
-                'hypermesh'
-            ],
+            searchRecord: [],
             searchValue: ''
         };
         this._toggleSearchPanel = this._toggleSearchPanel.bind(this);
@@ -29,7 +26,7 @@ export default class Navbar extends Component {
 
     _activateSelected(target = 0) {
         return this.props.selectedIdx == target ?
-            'bg-gray-900 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-white';
+            'bg-gray-900 text-white' : 'text-gray-700 hover:bg-indigo-100 hover:text-black';
     }
 
     _loginChecked() {
@@ -57,11 +54,12 @@ export default class Navbar extends Component {
     }
 
     render() {
-        const basicLinkStyle = ' px-3 py-2 rounded-md text-sm font-medium ';
+        const basicLinkStyle = ' px-3 py-2 rounded-md text-sm font-semibold font-mono ';
+        const { user } = this.props;
 
         return (
             <div className="fixed w-screen z-50">
-                <nav className="bg-white pb-2 border-b-2">
+                <nav className="bg-white pb-2 border-b">
                     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                         <div className="relative flex items-center justify-between h-16">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -73,27 +71,56 @@ export default class Navbar extends Component {
                                 <div className="flex-shrink-0 flex items-center -ml-20 relative -top-1">
                                     <Link prefetch href="/">
                                         <a className="mt-2 p-1 rounded-xl font-oswald">
-                                            <span className="p-4 relative top-1 text-black text-2xl font-bold cursor-pointer">BEPDPP</span>
+                                            <span className="p-4 relative top-1 text-black text-2xl font-bold cursor-pointer">BEDMoB</span>
                                         </a>
                                     </Link>
                                 </div>
                                 <div className="flex-auto flex items-center ml-10" onClick={this._toggleSearchPanel} onFocus={this._toggleSearchPanel}>
                                     <button className="relative top-1 group leading-6 font-medium flex items-center space-x-3 sm:space-x-4 text-gray-500 hover:text-black transition-colors duration-200 w-full py-2">
                                         <svg width="24" height="24" fill="none">
-                                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
                                         </svg>
                                         <span>查詢</span>
                                         <span className="hidden sm:block text-sm leading-5 py-0.5 px-1.5 mt-0.5 border border-gray-700 rounded-md font-oswald">
                                             <span className="sr-only">Press </span>
-                                            <kbd className="font-sans"><abbr title="Control" style={{ 'text-decoration': 'none' }}>Ctrl </abbr></kbd><span className="sr-only"> and </span><kbd className="font-sans">K</kbd><span className="sr-only"> to search</span>
+                                            <kbd className="font-sans"><abbr title="Control" style={{ textDecoration: 'none' }}>Ctrl </abbr></kbd><span className="sr-only"> and </span><kbd className="font-sans">K</kbd><span className="sr-only"> to search</span>
                                         </span>
                                     </button>
                                 </div>
                             </div>
-                            <div className="hidden mt-2 sm:block sm:ml-6">
-                                <div className="flex space-x-4 last:ml-20">
+                            <div className="hidden mt-2 sm:block sm:ml-6 relative sm:-right-16">
+                                <div className="flex space-x-4 last:ml-20" style={{ font: '400 13.3333px Arial' }}>
+                                    <Link prefetch href="/"><a className={this._activateSelected(0) + basicLinkStyle}>主頁</a></Link>
+                                    <Link prefetch href="/transaction/create">
+                                        <a className={this._activateSelected(1) + basicLinkStyle + (this._loginChecked() ? 'block' : 'hidden')}>
+                                            資料集登記
+                                        </a>
+                                    </Link>
+                                    <Link prefetch href="/profile">
+                                        <a className={this._activateSelected(2) + basicLinkStyle + (this._loginChecked() ? 'block' : 'hidden')}>
+                                            個人資料
+                                        </a>
+                                    </Link>
                                     <Link prefetch href="/login"><a className={this._activateSelected(-1) + basicLinkStyle + (this._loginChecked() ? 'hidden' : 'block')}>登入</a></Link>
-                                    <Link prefetch href="/logout"><a className={this._activateSelected(-1) + basicLinkStyle + (this._loginChecked() ? 'block' : 'hidden')}>{this.props.user.account} 登出</a></Link>
+                                    <Link prefetch href="/logout"><a className={this._activateSelected(-1) + basicLinkStyle + (this._loginChecked() ? 'block' : 'hidden')}>登出</a></Link>
+                                    <Link prefetch href="/signup"><a className={basicLinkStyle + (this._loginChecked() ? 'hidden' : 'block') + ' bg-blue-600 text-white hover:bg-blue-500'}>註冊</a></Link>
+                                    <span className={`${basicLinkStyle} ${this._loginChecked() ? 'block' : 'hidden'}`}>|</span>
+                                    {
+                                        /*
+                                        this._loginChecked() ?
+                                            <span className={basicLinkStyle}>
+                                                錢包地址：{user.account}
+                                            </span>
+                                            : null
+                                        */
+                                    }
+                                    {
+                                        this._loginChecked() ?
+                                            <span className={basicLinkStyle}>
+                                                餘額：{user.balance} ETH
+                                            </span>
+                                            : null
+                                    }
                                 </div>
                             </div>
                         </div>
@@ -115,14 +142,12 @@ export default class Navbar extends Component {
                             <div>
                                 <ul>
                                     {
-                                        this.state.searchRecord.filter((record) => {
-                                            return record === this.state.searchValue;
-                                        }).map((record) => {
+                                        this.state.searchRecord.map((record) => {
                                             return (
                                                 <li>
                                                     <Link>
                                                         <a>
-                                                            record
+                                                            {record}
                                                         </a>
                                                     </Link>
                                                 </li>
