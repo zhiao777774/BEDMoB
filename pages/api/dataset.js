733fb7bdedc77a718b7725101c385b920c3567fb
zhiao777774 <zhiaohsu@gmail.com>
2021-05-31 18:40:56 +0800

    新增/查詢/刪除資料集API

---
+++ pages/api/dataset.js
@@ -0,0 +1,40 @@
+import nextConnect from 'next-connect';
+import gas from '@/constants/contractMethodGas';
+import MongoDB from '@/database';
+import adapter from '@/api/database.adapter';
+import BIoTCM, { newContract } from '@/utils/factory';
+import Eth from 'ethjs';
+
+
+const db = new MongoDB({ dbName: 'bepdpp' });
+const handler = nextConnect();
+
+db.connect();
+handler.use(db.middleware)
+
+const collection = 'dataset';
+
+handler.get(adapter.GET(collection))
+    .delete(adapter.DELETE(collection));
+
+handler.post(async (req, res) => {
+    const { account, description, prices, boundedErrors } = req.body;
+
+    await req.db.collection(collection).insertOne({
+        owner: account,
+        description,
+        prices,
+        boundedErrors,
+        volume: 0
+    });
+
+    await BIoTCM.methods.dataOwnerCreateContentProduct(
+        description,
+        prices.map((price) => Eth.toWei(price, 'ether')),
+        boundedErrors
+    ).send({ from: account, gas: gas.dataOwnerCreateContentProduct });
+
+    return res.status(201).end();
+});
+
+export default handler;
