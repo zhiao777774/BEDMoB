dfc61339e1bcf14a116058866a85760f85b5ad59
zhiao777774 <zhiaohsu@gmail.com>
2021-06-02 02:31:02 +0800

    將currentProvider更改為ethereum

--- utils/web3utils.js
+++ utils/web3utils.js
@@ -12,8 +12,8 @@ export { web3 };
 // web3js wrap
 import Web3 from 'web3';
 
-const web3 = (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') ?
-    new Web3(window.web3.currentProvider) :
+const web3 = (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') ?
+    new Web3(window.ethereum) :
     new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 
 export { web3 };
