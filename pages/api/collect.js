3cc96a0838ae72148e08c17b58d8943c0a19ad70
zhiao777774 <zhiaohsu@gmail.com>
2021-06-02 22:19:03 +0800

    新增GET API、修正POST API錯誤

--- pages/api/collect.js
+++ pages/api/collect.js
@@ -7,29 +7,42 @@ const db = new MongoDB({ dbName: 'bepdpp' });
 const handler = nextConnect();
 
 db.connect();
-handler.use(db.middleware)
-    .post(async (req, res) => {
-        const { condition, update } = req.body;
+handler.use(db.middleware);
 
-        const bias = '^vfbvbtadso!mpy';
-        condition['account'] = md5(md5(condition['account'] + bias));
+handler.get(async (req, res) => {
+    const { account } = req.query;
 
-        const doc = await req.db.collection('watch').findOne({
-            account: condition['account']
-        });
+    const bias = '^vfbvbtadso!mpy';
 
-        if (doc) {
-            await req.db.collection('watch')
-                .updateOne(condition, { $set: update });
-        } else {
-            await req.db.collection('watch')
-                .insertOne({
-                    account: condition['account'],
-                    watchList: condition['watchList']
-                });
-        }
-
-        return res.status(201).end();
+    const doc = await req.db.collection('collection').findOne({
+        account: md5(md5(account + bias))
     });
+
+    res.json((doc && doc.watchList) || []);
+});
+
+handler.post(async (req, res) => {
+    const { condition, update } = req.body;
+
+    const bias = '^vfbvbtadso!mpy';
+    condition['account'] = md5(md5(condition['account'] + bias));
+
+    const doc = await req.db.collection('collection').findOne({
+        account: condition['account']
+    });
+
+    if (doc) {
+        await req.db.collection('collection')
+            .updateOne(condition, { $set: update });
+    } else {
+        await req.db.collection('collection')
+            .insertOne({
+                account: condition['account'],
+                watchList: update['watchList']
+            });
+    }
+
+    return res.status(201).end();
+});
 
 export default handler;
