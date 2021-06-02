f0b7dd246b895b4ba0975ba2f6b45be02fd634ac
zhiao777774 <zhiaohsu@gmail.com>
2021-06-02 09:09:31 +0800

    POST API添加Session使用(更新最新交易後的Balance結果)

--- pages/api/transaction.js
+++ pages/api/transaction.js
@@ -1,15 +1,17 @@
 import nextConnect from 'next-connect';
-import gas from '@/constants/contractMethodGas';
+import { ironSession } from 'next-iron-session';
+import cookieConfig from '@/constants/serverSideCookie';
 import MongoDB from '@/database';
 import adapter from '@/api/database.adapter';
-import BIoTCM, { web3 } from '@/utils/factory';
+import { web3 } from '@/utils/factory.js';
 
 
 const db = new MongoDB({ dbName: 'bepdpp' });
+const session = ironSession(cookieConfig);
 const handler = nextConnect();
 
 db.connect();
-handler.use(db.middleware)
+handler.use(db.middleware).use(session);
 
 const collection = 'transaction';
 
@@ -18,6 +20,20 @@ handler.get(adapter.GET(collection))
 
 handler.post(async (req, res) => {
     await req.db.collection(collection).insertOne(req.body);
+
+    const balance = await web3.eth.getBalance(req.body.consumer, 'latest');
+    let balanceWei = web3.utils.fromWei(balance.toString(), 'ether').toString();
+    balanceWei = parseFloat(balanceWei).toFixed(2);
+
+    const origin = req.session.get('user');
+    await req.session.destroy('user');
+
+    req.session.set('user', {
+        balance: balanceWei,
+        ...origin
+    });
+    await req.session.save();
+
     return res.status(201).end();
 });
 
