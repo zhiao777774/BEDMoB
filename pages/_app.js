b6cfe5848e3edcfbe0c508d343aaa6d0db9f1997
zhiao777774 <zhiaohsu@gmail.com>
2021-06-03 22:22:51 +0800

    調整網頁title

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
         <div>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <title>BEPDPP</title>
+                <title>BEDMoB</title>
             </Head>
             <Component {...pageProps} />
         </div>
