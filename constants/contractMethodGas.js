62ebae2cfe00c0058fa4aa4b024a116d53116474
zhiao777774 <zhiaohsu@gmail.com>
2021-06-03 21:10:14 +0800

    調整gas使用量

--- constants/contractMethodGas.js
+++ constants/contractMethodGas.js
@@ -1,7 +1,7 @@
 export default {
     dataOwnerCreateContentProduct: 281728,
-    consumerRegisterProduct: 148694,
-    purchaseProductContent: 75793,
+    consumerRegisterProduct: 190190,
+    purchaseProductContent: 77968,
     sendProductContent: 86491,
     queryProductContent: 28062
 };
