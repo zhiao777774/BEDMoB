411c68a8f5fd8c76087e1f86ab9b7b23f0b87778
zhiao777774 <zhiaohsu@gmail.com>
2021-06-03 20:34:26 +0800

    儲存至IPFS前先資料加密

--- api/ipfsAPI/ipfsAPI.js
+++ api/ipfsAPI/ipfsAPI.js
@@ -1,10 +1,16 @@
 import ipfsClient from 'ipfs-http-client';
+import { encryptLongContent } from '@/api/encryptAPI/encrptAPI';
 
 
-export async function upload(buffer) {
+export async function upload(content, publicKey = undefined) {
     const ipfs = new ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
-    const productContentAdded = await ipfs.add(buffer);
+    if (publicKey) {
+        content = encryptLongContent(publicKey, content);
+        content = Buffer.from(content, 'utf-8');
+    }
+
+    const productContentAdded = await ipfs.add(content);
     const productContentHash = productContentAdded.path;
 
     return {
