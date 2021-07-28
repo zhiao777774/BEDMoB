c4fa1378f1a36c21be9db477f2c07a27d5372db1
zhiao777774 <zhiaohsu@gmail.com>
2021-07-29 01:36:14 +0800

    從本地測試鏈調整為rinkeby測試鏈

--- utils/web3utils.js
+++ utils/web3utils.js
@@ -12,8 +12,9 @@ export { web3 };
 // web3js wrap
 import Web3 from 'web3';
 
+const infuraKey = '3a545e2e7a254b29bcba14031dfdae81';
 const web3 = (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') ?
     new Web3(window.ethereum) :
-    new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+    new Web3(new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/${infuraKey}`));
 
 export { web3 };
