6c9d88b79a0e83ab7f5112514ddcb188ad2437c4
zhiao777774 <zhiaohsu@gmail.com>
2021-09-01 22:03:56 +0800

    優化程式碼

--- api/encryptAPI/encrptAPI.js
+++ api/encryptAPI/encrptAPI.js
@@ -2,7 +2,7 @@ import JSEncrypt from 'node-jsencrypt';
 import MultisigHMAC from 'multisig-hmac';
 import crypto from 'crypto';
 import eccrypto from 'eccrypto';
-import { encryptLong, decryptLong } from '@/utils/encrypter';
+import { encryptLong, decryptLong } from '../../utils/encrypter';
 
 
 JSEncrypt.prototype.encryptLong = encryptLong;
@@ -22,23 +22,22 @@ export function decryptLongContent(privateKey, message) {
     return decrypter.decryptLong(message);
 }
 
-export function multiSigEncrypt(message, ownerKey = undefined, consumerKey = undefined) {
+export function multiSigEncrypt(message) {
     const data = Buffer.from(message);
     const multisigHmac = new MultisigHMAC();
 
-    const randomIndex = (max) => Math.floor(Math.random() * max) + 1;
-    const maxRange = Math.pow(2, 32) - 1;
-    
-    const k1 = multisigHmac.keygen(ownerKey || randomIndex(maxRange));
-    const k2 = multisigHmac.keygen(consumerKey || randomIndex(maxRange));
+    const k1 = multisigHmac.keygen(0);
+    const k2 = multisigHmac.keygen(1);
     const s1 = multisigHmac.sign(k1, data);
     const s2 = multisigHmac.sign(k2, data);
 
-    const signature = multisigHmac.combine([s1, s2]);
+    const signatures = [s1, s2];
+    const signature = multisigHmac.combine(signatures);
 
     return {
         keys: [k1, k2],
-        signature
+        signature,
+        signatures
     };
 }
 
@@ -50,22 +49,21 @@ export function multiSigVerify(message, keys, signature, threshold = 2) {
 }
 
 export async function ecdsaEncrypt(message, privateKey = undefined) {
-    privateKey ||= eccrypto.generatePrivate();
+    privateKey = privateKey || eccrypto.generatePrivate();
     const publicKey = eccrypto.getPublic(privateKey);
 
-    const msg = crypto.createHash('sha256').update(message).digest('hex');
-    const sig = await eccrypto.sign(privateKey, msg);
+    const msg = crypto.createHash('sha256').update(message).digest();
+    const signature = await eccrypto.sign(privateKey, msg);
 
-    return { publicKey, sig, msgHash: msg };
+    return { publicKey, signature, msgHash: msg };
 }
 
-export function ecdsaVerify(message, publicKey, sig) {
-    const msg = crypto.createHash('sha256').update(message).digest('hex');
-    const isValid = false;
+export async function ecdsaVerify(message, publicKey, sig) {
+    const msg = crypto.createHash('sha256').update(message).digest();
 
-    eccrypto.verify(publicKey, msg, sig)
-        .then(() => isValid = true)
-        .catch(() => isValid = false);
+    const isValid = await eccrypto.verify(publicKey, msg, sig)
+        .then(() => true)
+        .catch(() => false);
 
     return isValid;
 }
