90f5b99ef8fceb320191f49be182aa134479e6b9
zhiao777774 <zhiaohsu@gmail.com>
2021-09-01 21:59:25 +0800

    調整合約方法呼叫程式碼、新增BERLE演算法呼叫

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,8 +3,9 @@ import md5 from 'md5';
 import { ObjectId } from 'mongodb';
 import gas from '@/constants/contractMethodGas';
 import { upload } from '@/api/ipfsAPI/ipfsAPI';
+import { calculateBE } from '@/api/boundedErrorCalcAPI/boundedErrorCalcAPI';
 import MongoDB from '@/database';
-import BIoTCM from '@/utils/factory.js';
+import BIoTCM, { web3 } from '@/utils/factory.js';
 
 
 const db = new MongoDB({ dbName: 'bepdpp' });
@@ -16,18 +17,32 @@ handler.use(db.middleware);
 const collection = 'transaction';
 
 handler.post(async (req, res) => {
-    const { _id, data } = req.body;
-    const { content, productCount, consumer, owner } = data;
+    const { _id, data, privateKey } = req.body;
+    const { content, productCount, consumer, owner, boundedError } = data;
 
     const bias = '^vfbvbtadso!mpy';
     const accountDoc = await req.db.collection('account').findOne({
         account: md5(md5(consumer + bias))
     });
-    const ipfsRes = await upload(content, accountDoc.publicKey);
 
+    const result = calculateBE(content.split('\r\n').map((d) => Number(d)), boundedError * 0.01);
+    const ipfsRes = await upload(result, accountDoc.publicKey, privateKey);
+
+    /*
     await BIoTCM.methods.sendProductContent(
         productCount, ipfsRes.hash, consumer
     ).send({ from: owner, gas: gas.sendProductContent });
+    */
+
+    const methodData = await BIoTCM.methods.sendProductContent(
+        productCount, ipfsRes.hash, consumer
+    ).encodeABI();
+    const signedTx = await web3.eth.accounts.signTransaction(
+        { from: owner, to: BIoTCM.options.address, gas: gas.sendProductContent, data: methodData },
+        privateKey
+    );
+    const txResult = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    console.log(txResult);
 
     await req.db.collection(collection)
         .updateOne({ _id: new ObjectId(_id) }, {
@@ -40,7 +55,7 @@ handler.post(async (req, res) => {
 
     const doc = await req.db.collection('dataset')
         .findOne({ productCount });
-        
+
     req.db.collection('dataset')
         .updateOne({ productCount }, {
             $set: {
