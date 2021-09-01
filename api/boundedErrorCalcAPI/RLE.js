d4e31f1e58f3dd2f6d687cb5338d47d03ca946b1
zhiao777774 <zhiaohsu@gmail.com>
2021-09-01 20:53:55 +0800

    RLE類別初始化

---
+++ api/boundedErrorCalcAPI/RLE.js
@@ -0,0 +1,72 @@
+export class RLE {
+    static encode(text) {
+        return text.replace(/([ \w])\1+/g, (group, chr) => group.length + chr);
+    }
+
+    static compressText(text) {
+        let result = '';
+
+        if (text.length > 0) {
+            let count = 1;
+            let value = text[0];
+
+            for (let i = 1; i < text.length; ++i) {
+                var entry = text[i];
+
+                if (entry == value) {
+                    count += 1;
+                } else {
+                    result += count + '' + value;
+
+                    count = 1;
+                    value = entry;
+                }
+            }
+
+            result += count + '' + entry;
+        }
+
+        return result;
+    }
+
+    static compressArray(array) {
+        const result = [];
+
+        if (array.length > 0) {
+            let count = 1;
+            let value = array[0];
+
+            for (let i = 1; i < array.length; ++i) {
+                var entry = array[i];
+
+                if (entry == value) {
+                    count += 1;
+                } else {
+                    result.push(count);
+                    result.push(value);
+
+                    count = 1;
+                    value = entry;
+                }
+            }
+
+            result.push(count);
+            result.push(value);
+        }
+
+        return result;
+    }
+
+    static decode(text, withNumbers = false) {
+        if (withNumbers) return RLE.decodeArray(text.split(''));
+        return text.replace(/(\d+)([ \w])/g, (_, count, chr) => chr.repeat(count));
+    }
+
+    static decodeArray(array) {
+        let result = '';
+        for (let i = 0; i < array.length; i += 2)
+            result += array[i + 1].repeat(array[i]);
+
+        return result;
+    }
+}
