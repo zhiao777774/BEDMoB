ae5e81e924a55b7da745a71fcbee13228bd3f75c
zhiao777774 <zhiaohsu@gmail.com>
2021-09-16 01:38:28 +0800

    優化程式碼

--- utils/web3utils.js
+++ utils/web3utils.js
@@ -12,9 +12,9 @@ export { web3 };
 // web3js wrap
 import Web3 from 'web3';
 
-const infuraKey = '3a545e2e7a254b29bcba14031dfdae81';
+const { INFURA_KEY } = process.env;
 const web3 = (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') ?
     new Web3(window.ethereum) :
-    new Web3(new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/${infuraKey}`));
+    new Web3(new Web3.providers.HttpProvider(`https://rinkeby.infura.io/v3/${INFURA_KEY}`));
 
 export { web3 };
