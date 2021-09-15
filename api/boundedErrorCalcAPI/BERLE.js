aebf5e08d954cd268b791c1ad7a93b650d16f6b4
zhiao777774 <zhiaohsu@gmail.com>
2021-09-16 01:35:56 +0800

    優化程式碼、新增WatermarkBERLE類別

--- api/boundedErrorCalcAPI/BERLE.js
+++ api/boundedErrorCalcAPI/BERLE.js
@@ -1,5 +1,3 @@
-import { RLE } from './RLE';
-
 export class BERLE {
     constructor(boundedError = undefined) {
         this._boundedError = boundedError;
@@ -23,17 +21,20 @@ export class BERLE {
             throw new Error('data must be Number Array.');
         else if (typeof this._boundedError === 'undefined')
             throw new Error('Please set the Bounded-Error value.');
-        
+
         const [upperBounded, lowerBounded] = [
-            data.map((d) => d + this._boundedError),
-            data.map((d) => d - this._boundedError)
+            data.map((d) => d + d * this._boundedError),
+            data.map((d) => d - d * this._boundedError)
         ];
 
+        return this._execute(upperBounded, lowerBounded, data.length, postfix);
+    }
+
+    _execute(upperBounded, lowerBounded, dataSize, postfix) {
         let upperBound = upperBounded[0];
         let lowerBound = lowerBounded[0];
         let counter = 0;
         let index = 0;
-        let dataSize = data.length;
 
         const precisionRLE = [];
         const precisionResult = [];
@@ -87,5 +88,88 @@ export class BERLE {
         }
 
         return berleResult;
+    }
+}
+
+export class WatermarkBERLE extends BERLE {
+    constructor(boundedError = undefined, watermark) {
+        super(boundedError);
+        this._watermark = watermark;
+    }
+
+    static convertBinary(data) {
+        return data.split('').map((c) =>
+            c.charCodeAt().toString(2).padStart(8, '0'));
+    }
+
+    execute(data, postfix = ' ') {
+        const wmBinary = WatermarkBERLE.convertBinary(this._watermark);
+        const wmSize = wmBinary.length * 8 + wmBinary.length;
+
+        const BE = {};
+        const memoize = {};
+        const wmData = data.slice(0, wmSize).map((val, idx) => {
+            const bin = (idx && !((idx + 1) % 9)) ? (idx === wmSize - 1 ? '1' : '0')
+                : wmBinary[Math.floor(idx / 9)][idx % 9];
+
+            let encoded = val;
+            if (bin === '1') { // 1 -> odd
+                if (val % 2 === 0) encoded -= 1;
+            } else { // 0 -> even
+                if (val % 2 === 1) encoded -= 1;
+            }
+
+            memoize[idx] = bin;
+            BE[idx] = 1 - encoded / val;
+
+            return val;
+        });
+
+        const upperBounded = wmData.map((val, idx) => val + val * (this._boundedError - BE[idx]))
+            .concat(data.slice(wmSize).map((d) => d + d * this._boundedError));
+
+        const lowerBounded = wmData.map((val, idx) => val - val * (this._boundedError - BE[idx]))
+            .concat(data.slice(wmSize).map((d) => d - d * this._boundedError));
+
+        return this._execute(upperBounded, lowerBounded, data.length, postfix)
+            .split(postfix).map((el, idx) => {
+                if (memoize.hasOwnProperty(idx)) {
+                    let [count, val] = el.split(' ');
+
+                    if (memoize[idx] === '1') { // 1 -> odd
+                        if (val % 2 === 0) val -= 1;
+                    } else { // 0 -> even
+                        if (val % 2 === 1) val -= 1;
+                    }
+
+                    return count + ' ' + val;
+                }
+
+                return el;
+            }).join(postfix);
+    }
+
+    static decodeData(data) {
+        let counter = 1;
+        let binaryData = '';
+
+        let old = '';
+        for (const val of data) {
+            if (val === old) continue;
+
+            if (!(counter % 9)) {
+                if (Number(val) % 2 !== 0) break;
+            } else {
+                if (Number(val) % 2 === 0) binaryData += '0';
+                else binaryData += '1';
+            }
+
+            old = val;
+            counter++;
+        }
+
+        return String.fromCharCode(
+            ...binaryData.match(/[01]{8}/g).map((c) => parseInt(c, 2))
+        );
     }
 }
